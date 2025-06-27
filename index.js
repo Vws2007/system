14166232059b2dec384365e7cfa36df2de4f8880
Silva <150679268+Vws2007@users.noreply.github.com>
2025-06-26 22:18:26 -0300

    Arrumando aba Planilhas e Tela de Login.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
-function login() {
+function login(event) {
+    event.preventDefault(); // Impede o envio do formulário
     let email = document.getElementById("user").value;
     let password = document.getElementById("pass").value;
 
-    if (email == "admin" && password == "1234") {
-        window.location.href = "./webpage/index.html";
+    if (email === "" || password === "") {
+        alert("Preencha todos os campos!");
+        return; // Interrompe a função se algum campo estiver vazio
     }
-    else {
+
+    if (email === "admin" && password === "1234") {
+        window.location.href = "./webpage/index.html";
+    } else {
         alert("Email ou senha inválidos!");
     }
 }
---
+++ webpage/index.js
@@ -0,0 +1,10 @@
+function exibir() {
+    document.getElementById('content').style.display = 'block';
+    document.getElementById('hidden').style.display = 'block';
+    document.getElementById('exibe').style.display = 'none';
+}
+function ocultar() {
+    document.getElementById('content').style.display = 'none';
+    document.getElementById('hidden').style.display = 'none';
+    document.getElementById('exibe').style.display = 'block';
+}
